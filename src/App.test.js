fef781ed9757937ae35d17f88cfb408dcb556661
Julius Räihä <julius.raiha@gmail.com>
2017-11-01 09:48:05 +0200

    Fixed tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { ResponsiveProvider, Theme } from 'hsl-shared-components';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render((
+		<ResponsiveProvider>
+			<ThemeProvider theme={Theme}>
+				<App />
+			</ThemeProvider>
+		</ResponsiveProvider>
+	), div);
 });
