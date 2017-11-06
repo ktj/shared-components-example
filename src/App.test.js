6d8e9d1792d942a7dd6b5b20cdb92772e8b4c97e
Julius Räihä <julius.raiha@gmail.com>
2017-11-06 15:43:04 +0200

    Updated react to 16

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 import { ThemeProvider } from 'styled-components';
 import ResponsiveProvider from 'hsl-shared-components/lib/Utils/ResponsiveProvider';
 import Theme from 'hsl-shared-components/lib/Theme';
+import TestRenderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -14,4 +15,8 @@ it('renders without crashing', () => {
 			</ThemeProvider>
 		</ResponsiveProvider>
 	), div);
+  const json = TestRenderer.create(
+  	<span>abc</span>
+  ).toJSON();
+  expect(json).toMatchSnapshot();
 });
